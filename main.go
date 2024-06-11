ce7c42bbb0120a06e46faaf449699de6834c31bd
EndriGuma <andrewgouma@gmail.com>
2024-06-11 18:36:39 +0200

    added 10 sec

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// 100 a day ideally
+//
 //export email results sending or not see github for inspiration
 package main
 
@@ -85,7 +87,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 			
             //sendPersonalizedEmail("smtp.hostinger.com", "465", "info@bcbrillantemarketing.com", "Assembly3637997Ab,", "Test 1", "<div>Hello world!</div>", sendEmailTo)
            EmailErr = sendPersonalizedEmail(r.Form["smtpHost"][0], r.Form["smtpPort"][0], r.Form["email"][0], r.Form["password"][0], r.Form["subject"][0], r.Form["body"][0], sendEmailTo)
-		   time.Sleep(5 * time.Second)
+		   time.Sleep(10 * time.Second)
         }
     }
 
